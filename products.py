6600b8f80ecdd2a00905bee22656fb5445263afb
kuoanliao <kuoan791227@gmail.com>
2021-01-03 14:02:27 -0500

    add header

--- products.py
+++ products.py
@@ -16,3 +16,14 @@ print(products)
 #for loop 把清單中的東西一個個拿出來
 for p in products:
 	print(p[0],'的價格是', p[1])
+
+####################寫入檔案###################
+#逗點是分開用的
+#\n是換行的意思
+#csv 是存成excel模式
+#with 是自動close
+#encoding 是編碼的意思
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f. write('product,price\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')#f.write 才是真正寫入
