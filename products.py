cd0c728e41a63397dee689f05bba68a74049c47a
kuoanliao <kuoan791227@gmail.com>
2021-01-06 18:41:09 -0500

    add check file

--- products.py
+++ products.py
@@ -1,12 +1,26 @@
+import os #operatinf system 用這個作業系統來檢查檔案在不在
+
+
 #讀取檔案
 products = []
-with open('products.csv', 'r', encoding = 'utf-8')as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):#path 是模組的意思 isfile是功能
+	print('找到檔案了')
+	with open('products.csv', 'r', encoding = 'utf-8')as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案')	
+
+
+
+
+
+
+
 
 
 #讓使用者輸入
@@ -24,6 +38,8 @@ while True:
 
 print(products)
 
+
+
 #印出所有購買紀錄
 #for loop 把清單中的東西一個個拿出來
 for p in products:
@@ -33,7 +49,7 @@ for p in products:
 
 
 
-####################寫入檔案###################
+#寫入檔案
 #逗點是分開用的
 #\n是換行的意思
 #csv 是存成excel模式
